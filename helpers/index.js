952488de32905611f84702cba77f6bc3681ac212
sinawic <sinawic@hasebgostar.com>
2022-06-11 00:36:14 +0430

    email sending service completed

--- helpers/index.js
+++ helpers/index.js
@@ -1,4 +1,5 @@
 var crypto = require('crypto')
+var nodemailer = require('nodemailer')
 
 const connection_string = process.env.DB_CONNECTION_STRING,
   accessTokenSecret = process.env.ACCESS_TOKEN_SECRET
@@ -9,7 +10,40 @@ function sha1(val) {
   return shasum.digest('hex')
 }
 
+// this will try to send an email using node mailer
+// with source of the email read from env by default
+// and options as passed
+function sendMail(options) {
+  var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: options.from || process.env.SOURCE_EMAIL,
+      pass: process.env.SOURCE_PASSWORD
+    }
+  })
+
+  var mailOptions = {
+    from: options.from || process.env.SOURCE_EMAIL,
+    to: options.to,
+    subject: options.subject,
+    text: options.text,
+  }
+
+  // add attachments if any
+  if (options.attachments && options.attachments.length > 0) {
+    mailOptions.attachments = options.attachments.map(attachment => ({ path: attachment.path }))
+  }
+
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Email sent: ' + info.response);
+    }
+  })
+}
+
 
 module.exports = {
-  sha1, connection_string, accessTokenSecret
+  sha1, connection_string, accessTokenSecret, sendMail
 }
--- mailService.js
+++ mailService.js
@@ -1,6 +1,11 @@
 const { Email, Attachment } = require('./models/models')
+const { sendMail } = require('./helpers')
 
 
+// a service which listens to database changes
+// if detects any email with {sent=false}, it will send an email
+// this is done for simulating queueing
+// so that the data doesn't get lost in high scales
 const init = async () => {
   const i = process.env.INTERVAL || 5000
   console.log(`Mail Service is running every ${i / 1000} seconds...`)
@@ -9,7 +14,7 @@ const init = async () => {
 
     const email = await Email.aggregate([
       { $match: { sent: false } },
-      { $sort: { 'date_created': -1 } },
+      { $sort: { 'date_created': 1 } },
       { $limit: 1 },
       {
         $lookup: {
@@ -18,11 +23,25 @@ const init = async () => {
           foreignField: 'email',
           as: 'attachments'
         }
-      }
+      },
+      {
+        $lookup: {
+          from: 'rooms',
+          localField: 'room',
+          foreignField: '_id',
+          as: 'room'
+        }
+      }, { $unwind: '$room' }
     ])
 
     if (email.length > 0) {
-      console.log(email[0]._id)
+      const { to, subject, text, attachments, room } = email[0]
+      try {
+        sendMail({ to, subject, text, attachments, from: room.email })
+        await Email.updateOne({ _id: email[0]._id }, { sent: true })
+      } catch (err) {
+        console.log(err)
+      }
     } else
       console.log('No emails to send')
 
