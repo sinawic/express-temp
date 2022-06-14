859d51145ef7b2cb2bbefee16b55bb2366730050
sinawic <sinawic@hasebgostar.com>
2022-06-14 23:00:40 +0430

    separated models

---
+++ models/adminModels.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose')
+
+const RoomSchema = new mongoose.Schema({
+  date_created: { type: Date, default: new Date() },
+  name: { type: String, required: true },
+  email: { type: String, default: process.env.MAIN_EMAIL },
+  website: { type: String, required: true }
+})
+RoomSchema.index({ name: 1 }, { unique: true })
+const Room = mongoose.model('room', RoomSchema)
+
+
+const SupporterSchema = new mongoose.Schema({
+  date_created: { type: Date, default: new Date() },
+  username: { type: String, required: true },
+  password: String,
+  active: { type: Boolean, default: true },
+  room: { type: mongoose.Schema.Types.ObjectId, required: true, ref: 'room' }
+})
+SupporterSchema.index({ email: 1 }, { unique: true })
+const Supporter = mongoose.model('supporter', SupporterSchema)
+
+
+module.exports = {
+  Supporter, SupporterSchema, Room, RoomSchema
+}
---
+++ models/emailModels.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose')
+
+const AttachmentSchema = new mongoose.Schema({
+  originalname: String,
+  encoding: String,
+  mimetype: String,
+  destination: String,
+  filename: String,
+  path: String,
+  size: Number,
+  email: { type: mongoose.Schema.Types.ObjectId, required: true, ref: 'email' }
+})
+const Attachment = mongoose.model('attachment', AttachmentSchema)
+
+
+const EmailSchema = new mongoose.Schema({
+  date_created: { type: Date, default: new Date() },
+  to: { type: String, required: true },
+  subject: { type: String, required: true },
+  text: { type: String, required: true },
+  sent: { type: Boolean, default: false },
+  supporter: { type: mongoose.Schema.Types.ObjectId },
+  room: { type: mongoose.Schema.Types.ObjectId }
+})
+const Email = mongoose.model('email', EmailSchema)
+
+
+
+module.exports = {
+  Attachment, AttachmentSchema, Email, EmailSchema
+}
